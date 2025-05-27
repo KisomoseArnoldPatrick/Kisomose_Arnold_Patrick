818753afcb8738cc65f7c53984a3cf30066e7025
KisomoseArnoldPatrick <kisomosearnoldpatrick@students.mak.ac.ug>
2025-05-27 22:03:23 +0300

    created a file to demonstrate method resolution order

--- university.py
+++ university.py
@@ -52,11 +52,11 @@ if __name__ == "__main__":
     student = Student("Alice", 20, "22/U/10017/PS", "Computer Science")
     student.display_info()
 
-    print('=== Lecturer Information ===')
+    print('\n=== Lecturer Information ===')
     lecturer = Lecturer("Dr. Smith", 45,"2012/MAK/12", "Mathematics", 80000)
     lecturer.course_units = ["Calculus", "Linear Algebra"]
     lecturer.display_info()
 
-    print('=== Staff Information ===')
+    print('\n=== Staff Information ===')
     staff = Staff("John Doe", 30, "2023/MAK/100", "Administrative Assistant", 40000)
     staff.display_info()
